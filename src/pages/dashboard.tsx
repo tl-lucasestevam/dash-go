d457642d63e5ce0cfa5770026fb2455f233d3c7e
Lucas EStevam <dev.lucasestevam@gmail.com>
2022-01-24 10:26:47 -0300

    :sparkles: Add example graphics

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,6 +1,61 @@
-import { Flex } from "@chakra-ui/react";
+import { Box, Flex, SimpleGrid, Text, theme } from "@chakra-ui/react";
+import dynamic from "next/dynamic";
 import { NextPage } from "next";
 import { Header, Sidebar } from "../components";
+import { ApexOptions } from "apexcharts";
+const Chart = dynamic(() => import("react-apexcharts"), {
+  ssr: false,
+});
+
+const options: ApexOptions = {
+  chart: {
+    toolbar: {
+      show: false,
+    },
+    zoom: {
+      enabled: false,
+    },
+    foreColor: theme.colors.gray[500],
+  },
+  grid: {
+    show: false,
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  tooltip: {
+    enabled: false,
+  },
+  xaxis: {
+    type: "datetime",
+    axisBorder: {
+      color: theme.colors.gray[600],
+    },
+    axisTicks: {
+      color: theme.colors.gray[600],
+    },
+    categories: [
+      "2021-01-18T00:00:00.000Z",
+      "2021-01-19T00:00:00.000Z",
+      "2021-01-20T00:00:00.000Z",
+      "2021-01-21T00:00:00.000Z",
+      "2021-01-22T00:00:00.000Z",
+      "2021-01-23T00:00:00.000Z",
+      "2021-01-24T00:00:00.000Z",
+    ],
+  },
+  fill: {
+    opacity: 0.3,
+    type: "gradient",
+    gradient: {
+      shade: "dark",
+      opacityFrom: 0.9,
+      opacityTo: 0.3,
+    },
+  },
+};
+
+const series = [{ name: "series1", data: [31, 120, 10, 85, 96, 485, 1] }];
 
 const Dashboard: NextPage = () => {
   return (
@@ -8,6 +63,16 @@ const Dashboard: NextPage = () => {
       <Header />
       <Flex w="100%" my={6} maxWidth={1480} mx="auto" px={6}>
         <Sidebar />
+        <SimpleGrid flex={1} gap={4} minChildWidth="320px" align="flex-start">
+          <Box p={8} bg="gray.800" borderRadius={8} pb={4}>
+            <Text>Subscribes of the week</Text>
+            <Chart type="area" height={160} options={options} series={series} />
+          </Box>
+          <Box p={8} bg="gray.800" borderRadius={8} pb={4}>
+            <Text>Open Tax</Text>
+            <Chart type="area" height={160} options={options} series={series} />
+          </Box>
+        </SimpleGrid>
       </Flex>
     </Flex>
   );
