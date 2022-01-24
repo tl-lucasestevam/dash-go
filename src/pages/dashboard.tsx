737aeb9ce72d2c56d879c8a495a36ab7b0107f02
Lucas EStevam <dev.lucasestevam@gmail.com>
2022-01-24 09:47:21 -0300

    :sparkles: Add sidebar component

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,8 +1,16 @@
+import { Flex } from "@chakra-ui/react";
 import { NextPage } from "next";
-import { Header } from "../components";
+import { Header, Sidebar } from "../components";
 
 const Dashboard: NextPage = () => {
-  return <Header />;
+  return (
+    <Flex direction="column" h="100vh">
+      <Header />
+      <Flex w="100%" my={6} maxWidth={1480} mx="auto" px={6}>
+        <Sidebar />
+      </Flex>
+    </Flex>
+  );
 };
 
 export default Dashboard;
