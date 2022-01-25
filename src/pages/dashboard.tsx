6e1a804114934cfda1ff879bf702080cec9df585
Lucas EStevam <dev.lucasestevam@gmail.com>
2022-01-25 08:31:13 -0300

    :iphone: Add a responsive dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -64,11 +64,11 @@ const Dashboard: NextPage = () => {
       <Flex w="100%" my={6} maxWidth={1480} mx="auto" px={6}>
         <Sidebar />
         <SimpleGrid flex={1} gap={4} minChildWidth="320px" align="flex-start">
-          <Box p={8} bg="gray.800" borderRadius={8} pb={4}>
+          <Box p={["6", "8"]} bg="gray.800" borderRadius={8} pb={4}>
             <Text>Subscribes of the week</Text>
             <Chart type="area" height={160} options={options} series={series} />
           </Box>
-          <Box p={8} bg="gray.800" borderRadius={8} pb={4}>
+          <Box p={["6", "8"]} bg="gray.800" borderRadius={8} pb={4}>
             <Text>Open Tax</Text>
             <Chart type="area" height={160} options={options} series={series} />
           </Box>
