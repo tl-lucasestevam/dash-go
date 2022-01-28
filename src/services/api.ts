bb398babceca64a5f69c7e2e49f56aa572dbadbf
Lucas EStevam <dev.lucasestevam@gmail.com>
2022-01-28 08:00:29 -0300

    :rocket: Update axios base url to deploy

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api/",
+  baseURL:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000/api/"
+      : "https://dash-go-nine.vercel.app/api/",
 });
 
 export default api;
